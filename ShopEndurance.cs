3db573d05cd9b8235af91aeda942540d5f52a663
Stimayk <51941742+Stimayk@users.noreply.github.com>
2024-09-25 10:20:46 +0400

    Update ShopEndurance.cs

--- ShopEndurance.cs
+++ ShopEndurance.cs
@@ -10,7 +10,7 @@ namespace ShopEndurance
         public override string ModuleName => "[SHOP] Endurance";
         public override string ModuleDescription => "";
         public override string ModuleAuthor => "E!N";
-        public override string ModuleVersion => "v1.0.0";
+        public override string ModuleVersion => "v1.0.1";
 
         private IShopApi? SHOP_API;
         private const string CategoryName = "Endurance";
@@ -80,12 +80,13 @@ namespace ShopEndurance
             });
         }
 
-        public void OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName, int buyPrice, int sellPrice, int duration, int count)
+        public HookResult OnClientBuyItem(CCSPlayerController player, int itemId, string categoryName, string uniqueName, int buyPrice, int sellPrice, int duration, int count)
         {
             playerEndurances[player.Slot] = new PlayerEndurance(itemId);
+            return HookResult.Continue;
         }
 
-        public void OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
+        public HookResult OnClientToggleItem(CCSPlayerController player, int itemId, string uniqueName, int state)
         {
             if (state == 1)
             {
@@ -95,11 +96,13 @@ namespace ShopEndurance
             {
                 OnClientSellItem(player, itemId, uniqueName, 0);
             }
+            return HookResult.Continue;
         }
 
-        public void OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
+        public HookResult OnClientSellItem(CCSPlayerController player, int itemId, string uniqueName, int sellPrice)
         {
             playerEndurances[player.Slot] = null!;
+            return HookResult.Continue;
         }
 
         public record class PlayerEndurance(int ItemID);
